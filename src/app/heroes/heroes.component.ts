9254ae42516cf6cb00c4afd97bfb0b0272ede030
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-04-16 14:51:25 +0600

    Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 
 @Component({
@@ -11,11 +12,14 @@ import { Hero } from '../hero';
   styleUrl: './heroes.component.css', // The location of the component's private CSS styles.
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-} 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  }
+
 
 // Notes
 /*
